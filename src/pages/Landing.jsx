b5dd4dd323a137804890167aff8c9353b9237f45
LCSF2527 <c0964367@mylambton.ca>
2026-08-09 15:25:44 -0400

    fetch drinks

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,13 +1,22 @@
 import { useLoaderData } from 'react-router-dom'
+import axios from 'axios'
+
+const cocktailSearchUrl =
+  'https://www.thecocktaildb.com/api/json/v1/1/search.php?s='
 
 export const loader = async () => {
-  return 'something'
+  const searchTerm = 'margarita'
+
+  const response = await axios.get(`${cocktailSearchUrl}${searchTerm}`)
+
+  console.log(response)
+  return { drinks: response.data.drinks, searchTerm }
 }
 
 const Landing = () => {
-  const data = useLoaderData()
+  const { drinks, searchTerm } = useLoaderData()
 
-  console.log(data)
+  console.log(drinks)
 
   return <h1>Landing</h1>
 }
