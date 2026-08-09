f20f4ad1851af2ce4ec78b104c8cac81d1a52af5
LCSF2527 <c0964367@mylambton.ca>
2026-08-09 17:58:46 -0400

    single page error

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import {
   Cocktail,
   Newsletter,
   Error,
+  SinglePageError,
 } from './pages'
 
 import { loader as landingLoader } from './pages/Landing'
@@ -20,6 +21,7 @@ const router = createBrowserRouter([
       {
         index: true,
         element: <Landing />,
+        errorElement: <SinglePageError />,
         loader: landingLoader,
       },
       {
